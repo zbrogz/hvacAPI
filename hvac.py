ad155d29396d62c4de82ce0085dadde238f54979
Zach Brogan <zachbrogan@gmail.com>
2017-12-01 20:37:54 -0700

    Fixed hvac.py PEP8 formatting

--- hvac.py
+++ hvac.py
@@ -5,6 +5,8 @@ from uuid import uuid4 as Uuid
 import decimal
 
 # Helper class to convert a DynamoDB item to JSON.
+
+
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
@@ -14,17 +16,20 @@ class DecimalEncoder(json.JSONEncoder):
                 return int(o)
         return super(DecimalEncoder, self).default(o)
 
+
 def hvac_table():
     return boto3.resource('dynamodb').Table(os.environ['HVAC_TABLE_NAME'])
 
+
 def get_hvac(uuid):
-    hvac = hvac_table().get_item(Key={'uuid':uuid})
+    hvac = hvac_table().get_item(Key={'uuid': uuid})
     response = {
         'isBase64Encoded': 'false',
         'statusCode': 200,
         'body': json.dumps(hvac['Item'], cls=DecimalEncoder)
     }
     return response
+
 
 def get_all_hvacs():
     hvacs = hvac_table().scan()
@@ -35,18 +40,19 @@ def get_all_hvacs():
     }
     return response
 
+
 def create_hvac(hvac_data):
-    if not 'area' in hvac_data or not isinstance(hvac_data['area'], str):
+    if ('area' not in hvac_data or not isinstance(hvac_data['area'], str)):
         raise Exception('Error. Must specify area.')
     uuid = Uuid().hex
     hvac = {
         'uuid': uuid,
         'area': hvac_data['area'],
         'heater': False,
-        'ac':False,
-        'fan':False,
-        'off_time':0,
-        'update_period':30
+        'ac': False,
+        'fan': False,
+        'off_time': 0,
+        'update_period': 30
     }
     hvac_table().put_item(Item=hvac)
     response = {
@@ -55,6 +61,7 @@ def create_hvac(hvac_data):
         'body': json.dumps(hvac)
     }
     return response
+
 
 def update_hvac(uuid, hvac_data):
     updateExpressions = []
@@ -74,17 +81,18 @@ def update_hvac(uuid, hvac_data):
     if 'off_time' in hvac_data and isinstance(hvac_data['off_time'], int):
         updateExpressions.append("off_time = :o")
         attributeValues[':o'] = hvac_data['off_time']
-    if 'update_period' in hvac_data  and isinstance(hvac_data['update_period'], int):
+    if ('update_period' in hvac_data and
+            isinstance(hvac_data['update_period'], int)):
         updateExpressions.append("update_period = :u")
         attributeValues[':u'] = hvac_data['update_period']
 
     if len(updateExpressions) < 1:
         raise Exception('Error. Invalid update request.')
 
-    updateExpressionStr = "set "+(",".join(updateExpressions))
+    updateExpressionStr = "set " + (",".join(updateExpressions))
 
     hvac_table().update_item(
-        Key={'uuid':uuid},
+        Key={'uuid': uuid},
         UpdateExpression=updateExpressionStr,
         ExpressionAttributeValues=attributeValues)
     response = {
@@ -94,14 +102,16 @@ def update_hvac(uuid, hvac_data):
     }
     return response
 
+
 def delete_hvac(uuid):
-    hvac_table().delete_item(Key={'uuid':uuid})
+    hvac_table().delete_item(Key={'uuid': uuid})
     response = {
-      "isBase64Encoded": "false",
-      "statusCode": 200,
-      "body": "{\"message\": \"Hvac deleted.\"}"
+        "isBase64Encoded": "false",
+        "statusCode": 200,
+        "body": "{\"message\": \"Hvac deleted.\"}"
     }
-    return response 
+    return response
+
 
 def lambda_handler(event, context):
     try:
@@ -115,11 +125,14 @@ def lambda_handler(event, context):
             if event['body'] and not event['pathParameters']:
                 return create_hvac(json.loads(event['body']))
             else:
-                raise Exception('Error. HTTP body required for POST to create hvac.')
+                raise Exception(
+                    'Error. HTTP body required for POST to create hvac.')
 
         elif event['httpMethod'] == "PUT":
-            if event['body'] and event['pathParameters'] and 'uuid' in event['pathParameters']:
-                return update_hvac(event['pathParameters']['uuid'], json.loads(event['body']))
+            if (event['body'] and event['pathParameters'] and
+                    'uuid' in event['pathParameters']):
+                return update_hvac(event['pathParameters']['uuid'],
+                                   json.loads(event['body']))
 
         elif event['httpMethod'] == "DELETE":
             if event['pathParameters'] and 'uuid' in event['pathParameters']:
@@ -128,6 +141,6 @@ def lambda_handler(event, context):
         response = {
             "isBase64Encoded": "false",
             "statusCode": 400,
-            "body": "{\"errorMessage\": \""+e.args[0]+".\"}"
+            "body": "{\"errorMessage\": \"" + e.args[0] + ".\"}"
         }
         return response
